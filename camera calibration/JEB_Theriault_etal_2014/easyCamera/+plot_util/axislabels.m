15751a244ee2210c49612d48a62c0e70412c03f8
Sam Whitehead <samcwhitehead@gmail.com>
2021-03-14 18:14:05 -0400

    removed easyWand from repo

--- camera calibration/JEB_Theriault_etal_2014/easyCamera/+plot_util/axislabels.m
+++
@@ -1,5 +0,0 @@
-function [LX LY LZ] = axislabels(XLabel, YLabel, ZLabel)
-LX = xlabel(XLabel);
-LY = ylabel(YLabel);
-LZ = zlabel(ZLabel);
-end
--- camera calibration/JEB_Theriault_etal_2014/easyWand5/tutorial/README.txt
+++
@@ -1,1 +0,0 @@
-Data for the easyWand5 tutorial included in the end of the documentation.
