60da2be3c9126b65a6493385af782e78304e52eb
bkemperludlow <bkl48@cornell.edu>
2024-08-21 16:48:08 -0400

    fixed a bug in fitControllerGains

--- controller fitting/fitControllerGains.m
+++ controller fitting/fitControllerGains.m
@@ -69,10 +69,7 @@ ub = [] ; %[ 2  0.1  4];
 % ---------------------------------------------------------------------
 %% perform fit and assign outputs 
 [x, resnorm, residual, ~, ~, ~, J] = ...
-
-    lsqcurvefit(controllerEq, paramGuess, bodyData,wingAngleVals',...
-
-
+    lsqcurvefit(controllerEq, paramGuess, bodyData,wingAngleVals,...
     lb,ub,options) ;
 
 % ----------------------------------------------------------------
